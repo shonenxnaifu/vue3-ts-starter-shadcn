3ebd1f87d70a56809bd5d7f3a997d9eabd6125ec
Pawitra Warda <pawitrawarda@gmail.com>
2025-12-25 20:19:57 +0700

    update table commponent and change layou

---
+++ src/modules/dashboard/components/datatable/columns.ts
@@ -0,0 +1,77 @@
+import type { ColumnDef } from '@tanstack/vue-table'
+import { h } from 'vue'
+import DropdownAction from '../datatable/DataTableDropDown.vue'
+
+export interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+export const payments: Payment[] = [
+  {
+    id: 'm5gr84i9',
+    amount: 316,
+    status: 'success',
+    email: 'ken99@yahoo.com',
+  },
+  {
+    id: '3u1reuv4',
+    amount: 242,
+    status: 'success',
+    email: 'Abe45@gmail.com',
+  },
+  {
+    id: 'derv1ws0',
+    amount: 837,
+    status: 'processing',
+    email: 'Monserrat44@gmail.com',
+  },
+  {
+    id: '5kma53ae',
+    amount: 874,
+    status: 'success',
+    email: 'Silas22@gmail.com',
+  },
+  {
+    id: 'bhqecj4p',
+    amount: 721,
+    status: 'failed',
+    email: 'carmella@hotmail.com',
+  },
+]
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'email',
+  },
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  },
+  {
+    accessorKey: 'status',
+  },
+  {
+    id: 'actions',
+    header: () => h('div', { class: 'text-left' }, 'Action'),
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original
+
+      return h('div', { class: 'relative' }, h(DropdownAction, {
+        payment,
+      }))
+    },
+  },
+]
--- src/modules/dashboard/pages/Dashboard.vue
+++ src/modules/dashboard/pages/Dashboard.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import type { Payment } from '../components/datatable/columns'
+import { onMounted, ref } from 'vue'
+import { columns, payments } from '../components/datatable/columns'
+import Datatable from '../components/datatable/DataTable.vue'
 import SectionCards from '../components/SectionCards.vue'
+
+const data = ref<Payment[]>([])
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return payments
+}
+
+onMounted(async () => {
+  data.value = await getData()
+})
 </script>
 
 <template>
   <SectionCards />
+  <Datatable :columns="columns" :data="data" />
 </template>
