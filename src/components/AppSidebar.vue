f91f3c0830912676e567a21b177862c6e6e560fc
Pawitra Warda <pawitrawarda@gmail.com>
2026-04-16 14:41:42 +0700

    update menus

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -54,7 +54,7 @@ const data = {
     },
   ],
   navMenu: {
-    platform: [
+    pages: [
       {
         title: 'Dashboard',
         url: '/',
@@ -75,106 +75,17 @@ const data = {
           },
         ],
       },
-      {
-        title: 'Playground',
-        url: '#',
-        icon: SquareTerminal,
-        items: [
-          {
-            title: 'History',
-            url: '#',
-          },
-          {
-            title: 'Starred',
-            url: '#',
-          },
-          {
-            title: 'Settings',
-            url: '#',
-          },
-        ],
-      },
-      {
-        title: 'Models',
-        url: '#',
-        icon: Bot,
-        items: [
-          {
-            title: 'Genesis',
-            url: '#',
-          },
-          {
-            title: 'Explorer',
-            url: '#',
-          },
-          {
-            title: 'Quantum',
-            url: '#',
-          },
-        ],
-      },
-      {
-        title: 'Documentation',
-        url: '#',
-        icon: BookOpen,
-        items: [
-          {
-            title: 'Introduction',
-            url: '#',
-          },
-          {
-            title: 'Get Started',
-            url: '#',
-          },
-          {
-            title: 'Tutorials',
-            url: '#',
-          },
-          {
-            title: 'Changelog',
-            url: '#',
-          },
-        ],
-      },
-      {
-        title: 'Settings',
-        url: '#',
-        icon: Settings2,
-        items: [
-          {
-            title: 'General',
-            url: '#',
-          },
-          {
-            title: 'Team',
-            url: '#',
-          },
-          {
-            title: 'Billing',
-            url: '#',
-          },
-          {
-            title: 'Limits',
-            url: '#',
-          },
-        ],
-      },
     ],
-    projects: [
+    components: [
       {
-        title: 'Design Engineering',
+        title: 'Calendar',
         url: '#',
         icon: Frame,
       },
       {
-        title: 'Sales & Marketing',
+        title: 'Range Calendar',
         url: '#',
         icon: PieChart,
-      },
-      {
-        title: 'Travel',
-        url: '#',
-        icon: Map,
       },
     ],
   },
